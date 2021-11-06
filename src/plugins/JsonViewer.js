e42b15956a413b36d417534badcaffe00f62b992
Mr.YanG <yangyonghai9125@hotmail.com>
2021-11-06 17:06:12 +0800

    refactor

--- src/plugins/JsonViewer.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import JsonViewer from 'vue-json-viewer'
-
-Vue.use(JsonViewer)
--- src/plugins/diff.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import * as Diff from '../../node_modules/diff/dist/diff.js'
-
-Vue.prototype.$diff = Diff
